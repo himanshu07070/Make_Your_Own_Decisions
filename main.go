a43103ec84c9494c0206105042c5b8de5fbbd4cc
himanshu07070 <36814920+himanshu07070@users.noreply.github.com>
2021-06-03 13:42:38 +0530

    Update main.go

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
 		panic(err)
 	}
 
-	h := myod.NewHandler(story, nil)
+	h := myod.NewHandler(story)
 	fmt.Printf("Started server at Port %d\n", *port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 
